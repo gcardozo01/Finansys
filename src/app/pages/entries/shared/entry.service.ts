7a8f70384c73c3ac727d8fb554b9202e54eaa443
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-12-08 14:09:38 -0300

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,13 +1,12 @@
 // Angular
 import { Injectable, Injector } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 
 // Models
 import { Entry } from "./entry.model";
 
 // RXJS
 import { Observable, pipe } from "rxjs";
-import { flatMap } from "rxjs/operators";
+import { catchError, flatMap } from "rxjs/operators";
 
 // Services
 import { CategoryService } from "../../categories/shared/category.service";
@@ -25,24 +24,24 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap((category) => {
-        entry.category = category;
-
-        return super.create(entry);
-      })
-    );
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   update(entry: Entry): Observable<Entry> {
-    const url = `${this.apiPath}/${entry.id}`;
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
 
+  private setCategoryAndSendToServer(
+    entry: Entry,
+    sendFn: any
+  ): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap((category) => {
         entry.category = category;
 
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     );
   }
 }
