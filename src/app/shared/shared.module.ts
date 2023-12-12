ba6e657b8f796fc8ebf50254d086423c8622f3be
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-12-12 17:31:40 -0300

    adding FormFieldErrorComponent to Forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,15 +7,22 @@ import { RouterModule } from "@angular/router";
 // Components
 import { BreadCrumbComponent } from "./components/bread-crumb/bread-crumb.component";
 import { PageHeaderComponent } from "./components/page-header/page-header.component";
+import { FormFieldErrorComponent } from "./components/form-field-error/form-field-error.component";
 
 @NgModule({
-  declarations: [BreadCrumbComponent, PageHeaderComponent],
+  declarations: [
+    BreadCrumbComponent,
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+  ],
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
     PageHeaderComponent,
+
+    FormFieldErrorComponent,
     RouterModule,
   ],
 })
